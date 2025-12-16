ac99b9c0c60a9cabe9ff75ea1e2a589c53bc38a8
Ganapathy-dev <ganapathy@testpress.in>
2025-12-16 17:19:00 +0530

    misc styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
+import path from "path";
 
 export default defineConfig({
   base: "/yolo-proctor/",
-  server: {
-
+  server: {},
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        temp: path.resolve(__dirname, "temp.html"),
+      },
+    },
   },
   optimizeDeps: {
     exclude: ['onnxruntime-web'],
